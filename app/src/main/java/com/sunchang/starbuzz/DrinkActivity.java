5909a2f5b567448b05adbac772a8135a57e353a3
JustinSun <justinsun@yeah.net>
2018-03-12 17:39:39 +0800

    增加favorite视图列表。

--- app/src/main/java/com/sunchang/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/sunchang/starbuzz/DrinkActivity.java
@@ -1,5 +1,6 @@
 package com.sunchang.starbuzz;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
@@ -7,6 +8,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +24,8 @@ public class DrinkActivity extends AppCompatActivity {
 
     private TextView description;
 
+    private CheckBox favorite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,24 +35,46 @@ public class DrinkActivity extends AppCompatActivity {
 
         try {
             SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
-            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
-            Cursor cursor = db.query("DRINK", new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"}, "_id = ?",
-                    new String[]{Integer.toString(drinkNo)}, null, null, null);
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkNo)},
+                    null, null, null);
             if (cursor.moveToFirst()) {
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
 
                 photo = (ImageView) this.findViewById(R.id.photo);
                 name = (TextView) this.findViewById(R.id.name);
                 description = (TextView) this.findViewById(R.id.description);
+                favorite = (CheckBox) this.findViewById(R.id.favorite);
 
                 name.setText(nameText);
                 description.setText(descriptionText);
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
+                favorite.setChecked(isFavorite);
             }
             cursor.close();
+            db.close();
+        } catch (SQLException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onFavoriteClick(View view) {
+        int drinkNo = this.getIntent().getIntExtra(this.EXTRA_DRINKNO, 0);
+        favorite = (CheckBox) this.findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("FAVORITE", favorite.isChecked());
+
+        try {
+            SQLiteOpenHelper sqLiteOpenHelper = new StarbuzzDatabaseHelper(this);
+            SQLiteDatabase db = sqLiteOpenHelper.getWritableDatabase();
+            db.update("DRINK", drinkValues, "_id = ?", new String[]{Integer.toString(drinkNo)});
             db.close();
         } catch (SQLException e) {
             Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/sunchang/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/sunchang/starbuzz/TopLevelActivity.java
@@ -1,15 +1,26 @@
 package com.sunchang.starbuzz;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends AppCompatActivity {
 
     private ListView listView;
+
+    private SQLiteDatabase db;
+
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +37,55 @@ public class TopLevelActivity extends AppCompatActivity {
                 }
             }
         });
+
+        ListView listFavorites = (ListView) this.findViewById(R.id.list_favorites);
+        try {
+            SQLiteOpenHelper sqLiteOpenHelper = new StarbuzzDatabaseHelper(this);
+            db = sqLiteOpenHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINK", new String[]{"_id", "NAME"}, "FAVORITE = 1",
+                    null, null, null, null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listFavorites.setAdapter(favoriteAdapter);
+        } catch (SQLException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) l);
+                TopLevelActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        try {
+            SQLiteOpenHelper sqLiteOpenHelper = new StarbuzzDatabaseHelper(this);
+            db = sqLiteOpenHelper.getReadableDatabase();
+            Cursor newCursor = db.query("DRINK", new String[]{"_id", "NAME"}, "FAVORITE = 1",
+                    null, null, null, null);
+            ListView listFavorites = (ListView) this.findViewById(R.id.list_favorites);
+            CursorAdapter adapter = (CursorAdapter) listFavorites.getAdapter();
+            adapter.changeCursor(newCursor);
+            favoritesCursor = newCursor;
+        } catch (SQLException e) {
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
     }
 }
